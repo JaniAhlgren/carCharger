b4a60f283380cc2ee95790b529bbafbad294a310
Jani Ahlgren <jani.ahlgren@qubilea.fi>
2022-02-02 07:39:03 +0200

    remove unnecessary json parser from json handler

--- jsonHandler.py
+++ jsonHandler.py
@@ -1,4 +1,5 @@
 from asyncio.log import logger
+from asyncio.windows_events import NULL
 import sys
 from urllib import response
 import requests
@@ -11,7 +12,7 @@ from logger import chargerLogger as cl
 class getJson():
     API_URL = 'https://www.nordpoolgroup.com/api/marketdata/page/%i'
     
-    def _fetch_json(self, data_type, end_date=None):
+    def _fetch_nordpool_json(self, data_type, end_date=None):
         ''' Fetch JSON from API '''
         # If end_date isn't set, default to tomorrow
         if end_date is None:
@@ -27,75 +28,26 @@ class getJson():
         })
         # Return JSON response
         return(r.content)
-    def _parse_json(self, data, areas=[]):
-        '''
-            Parse json response from fetcher.
-            Returns dictionary with
-                - start time
-                - end time
-                - update time
-                - currency
-                - dictionary of areas, based on selection
-                    - list of values (dictionary with start and endtime and value)
-                    - possible other values, such as min, max, average for hourly
-            '''
+    def getTomorrowsElectricityPrice(json, carunaNightTime=["22","23","0","1","2","3","4","5","6"], NGE_Margin=0.16):
+        listedPrices=[]
+        carunaNightPrice=2.74
+        carunaDayPrice=4.45
+        for y in range(24):
+            startTime=json["data"]["Rows"][y]["StartTime"]
+            # this should be used to verify that the time is rightprint(x["data"]["Rows"][0]["Columns"][0]["Name"])
+            price=json["data"]["Rows"][y]["Columns"][0]["Value"]
 
-        # If areas isn't a list, make it one
-        if not isinstance(areas, list):
-            areas = list(areas)
-
-        # Update currency from data
-        currency = str(data[0])
-        print(currency)
-        # Ensure that the provided currency match the requested one
-        if currency != self.currency:
-            raise Exception
-
-        # All relevant data is in data['data']
-        data = data['data']
-        start_time = self._parse_dt(data['DataStartdate'])
-        end_time = self._parse_dt(data['DataEnddate'])
-        updated = self._parse_dt(data['DateUpdated'])
-
-        area_data = {}
-        # Loop through response rows
-        for r in data['Rows']:
-            row_start_time = self._parse_dt(r['StartTime'])
-            row_end_time = self._parse_dt(r['EndTime'])
-
-            # Loop through columns
-            for c in r['Columns']:
-                name = c['Name']
-                # If areas is defined and name isn't in areas, skip column
-                if areas and name not in areas:
-                    continue
-
-                # If name isn't in area_data, initialize dictionary
-                if name not in area_data:
-                    area_data[name] = {
-                        'values': [],
-                    }
-
-                # Time based and average, max, min etc rows are separated
-                # with 'IsExtraRow' -marking
-                if r['IsExtraRow']:
-                    # Update extra data to dictionary
-                    area_data[name][r['Name']] = self._conv_to_float(c['Value'])
-                else:
-                    # Append dictionary to value list
-                    area_data[name]['values'].append({
-                        'start': row_start_time,
-                        'end': row_end_time,
-                        'value': self._conv_to_float(c['Value']),
-                    })
-
-        return {
-            'start': start_time,
-            'end': end_time,
-            'updated': updated,
-            'currency': currency,
-            'areas': area_data
-        }  
-        
-
+            #convert price to float
+            price=price.replace(',', '.').replace(" ", "")
+            startHour=datetime.fromisoformat(startTime).hour
+            hourlyPrice=NULL
+            if str(startHour) in carunaNightTime:
+                totalPrice=float(price)/10+float(NGE_Margin)+float(carunaNightPrice)
+                        
+            else:
+                totalPrice=float(price)/10+float(NGE_Margin)+float(carunaDayPrice)
+            hourlyPrice=f"{startTime} : {totalPrice}"
+            listedPrices.append(hourlyPrice)
+        cl.logWriter(listedPrices)
+        return listedPrices   
 
